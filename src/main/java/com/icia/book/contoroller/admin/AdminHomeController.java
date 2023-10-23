597cb84ed0f3f287b34e26ea2e171da8ff557158
YuPhe1 <oksunboy1@naver.com>
2023-10-23 10:36:10 +0900

    admin package 생성

--- src/main/java/com/icia/book/contoroller/admin/AdminHomeController.java
+++ src/main/java/com/icia/book/contoroller/admin/AdminHomeController.java
@@ -1,4 +1,4 @@
-package com.icia.book.contoroller;
+package com.icia.book.contoroller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
+public class AdminHomeController {
 
     @GetMapping
     public String adminMain(){
