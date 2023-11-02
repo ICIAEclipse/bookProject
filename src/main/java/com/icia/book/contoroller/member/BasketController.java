cfd06f04ae3c7c57472443c2bbe437deb56dff72
ScarsIntoStars <conpana_jj@naver.com>
2023-11-02 17:42:41 +0900

    basket update ver.03

--- src/main/java/com/icia/book/contoroller/member/BasketController.java
+++ src/main/java/com/icia/book/contoroller/member/BasketController.java
@@ -20,11 +20,21 @@ public class BasketController {
     private final BasketService basketService;
 
     @PostMapping
-    public ResponseEntity basket (@RequestBody BasketDTO basketDTO) {
+    public ResponseEntity basket(@RequestBody BasketDTO basketDTO) {
 
         try {
-            basketService.save(basketDTO.getIsbn(), basketDTO.getMemberEmail());
-            return new ResponseEntity<>("basket", HttpStatus.OK);
+            boolean check = basketService.findById(basketDTO.getIsbn(), basketDTO.getMemberEmail());
+            if (check == true) { // 있음
+                // DB에서 기존의 것 제거
+                basketService.delete(basketDTO.getIsbn(), basketDTO.getMemberEmail());
+                return new ResponseEntity<>("basket", HttpStatus.OK);
+            } else { // 없음
+                // DB에 추가
+                basketService.save(basketDTO.getIsbn(), basketDTO.getMemberEmail());
+                return new ResponseEntity<>("basket", HttpStatus.OK);
+            }
+
+
         } catch (NoSuchElementException e) {
             return new ResponseEntity<>("책 또는 회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
         }
