45995cfcb56059b96b211fb2c963abb3a750f92b
YuPhe1 <oksunboy1@naver.com>
2023-11-08 14:03:04 +0900

    faq List 출력, 페이징, 조회수 증가

--- src/main/java/com/icia/book/contoroller/cscenter/CsCenterController.java
+++ src/main/java/com/icia/book/contoroller/cscenter/CsCenterController.java
@@ -1,14 +1,19 @@
 package com.icia.book.contoroller.cscenter;
 
 import com.icia.book.dto.CscenterCategoryDTO;
+import com.icia.book.dto.FaqDTO;
 import com.icia.book.service.CsCenterCategoryService;
+import com.icia.book.service.FaqService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,11 +21,25 @@ import java.util.List;
 public class CsCenterController {
 
     private final CsCenterCategoryService csCenterCategoryService;
+    private final FaqService faqService;
 
     @GetMapping("/faq")
     public String findAll(Model model){
         List<CscenterCategoryDTO> csCenterCategoryDTOList = csCenterCategoryService.findAll();
         model.addAttribute("csCenterCategoryList", csCenterCategoryDTOList);
         return "csCenter/notice";
+    }
+
+    @GetMapping("/faq/list")
+    public ResponseEntity findFaqList(@RequestParam("id") Long id, @RequestParam("page") int page) {
+        Page<FaqDTO> faqDTOList = faqService.findAllById(id, page);
+        return new ResponseEntity(faqDTOList, HttpStatus.OK);
+    }
+
+    @PutMapping("/faq/upHits")
+    public ResponseEntity faqUpHits(@RequestBody Map<String, Long> requestBody){
+        Long id = requestBody.get("id");
+        faqService.upHits(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/icia/book/repository/FaqRepository.java
+++ src/main/java/com/icia/book/repository/FaqRepository.java
@@ -5,8 +5,15 @@ import com.icia.book.entity.FaqEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FaqRepository extends JpaRepository<FaqEntity, Long> {
+
+    @Modifying
+    @Query(value = "update FaqEntity f set f.faqHits = f.faqHits+1 where f.id=:id")
+    void upHits(@Param("id") Long id);
     Page<FaqEntity> findByCscenterCategoryEntity(CsCenterCategoryEntity csCenterCategoryEntity, PageRequest pageRequest);
 
     Page<FaqEntity> findByFaqTitleContaining(String q, PageRequest pageRequest);
